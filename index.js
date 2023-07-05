d70138a2a12af7b20e3624b7062381cd59cc4d23
Malick Roungou <mjbroungou@gmail.com>
2023-07-05 14:06:31 -0400

    interactiveness included

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function displayAnswer(questionId) {
-    let answer = document.getElementById("answer" + questionId);
-
-    
-}
+document.addEventListener('DOMContentLoaded', function() {
+    let questions = document.getElementsByClassName('concern');
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].addEventListener('click', function() {
+        this.classList.toggle('expanded');
+      });
+    }
+  });
+  
